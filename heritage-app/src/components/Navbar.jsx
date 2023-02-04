521905fac5006ff7818f0f9efd3c1d87f9a2bf86
Carsten Meyke <carsten.meyke@gmail.com>
2023-02-04 13:44:47 +0100

    Show wallet and contract balance in navbar

--- heritage-app/src/components/Navbar.jsx
+++ heritage-app/src/components/Navbar.jsx
@@ -13,6 +13,8 @@ import {
   ModalCloseButton,
   FormControl,
   Input,
+  Text,
+  Box,
 } from '@chakra-ui/react'
 import {
   MoonIcon,
@@ -25,16 +27,19 @@ import { ethers } from "ethers";
 
 import Heritage from '../artifacts/contracts/Heritage.sol/Heritage.json';
 
-async function connectWallet(setSigner, setAddress) {
+async function connectWallet(setSigner, setAddress, setWalletBalance, setProvider) {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   await provider.send("eth_requestAccounts", []);
   const signer = provider.getSigner();
   const address = await signer.getAddress();
+  const balance = await getEthBalance(signer, null);
+  setWalletBalance(balance);
   setSigner(signer);
+  setProvider(provider);
   setAddress(address);
 }
 
-async function connectContract(signer, contract, setContract, contractAddress, setContractAddress) {
+async function connectContract(signer, contract, setContract, contractAddress, setContractBalance, provider, setRole, address) {
   if (signer !== null && ethers.utils.isAddress(contractAddress)) {
     if (contract !== null) {
       const address = await contract.address;
@@ -46,17 +51,26 @@ async function connectContract(signer, contract, setContract, contractAddress, s
 
     const heritage = new ethers.Contract(contractAddress, Heritage.abi, signer);
     setContract(heritage);
+    const balance = await getEthBalance(null, provider, contractAddress);
+    setContractBalance(balance);
+    const APPOINTER_ROLE = await heritage.APPOINTER_ROLE();
+    const HEIR_ROLE = await heritage.HEIR_ROLE();
+    if (await heritage.hasRole(APPOINTER_ROLE, address)) {
+      setRole("Appointer");
+    } else if (await heritage.hasRole(HEIR_ROLE, address)) {
+      setRole("Heir");
+    }        
   } else {
     setContract(null);
   }
 }
 
 function DisplayContractAddress({contract, contractAddress}) {
-  const address = displayAddress(contractAddress);
-  return <>{contract === null || address === "" ? "Contract" : address}</>;
+  const address = formatAddress(contractAddress);
+  return <>{contract === null || address === "" ? "Connect" : address}</>;
 }
 
-function ContractButton({signer, contract, setContract, contractAddress, setContractAddress}) {
+function ContractButton({signer, contract, setContract, contractAddress, setContractAddress, setContractBalance, provider, setRole, address}) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const addressRef = useRef(null);
 
@@ -80,7 +94,7 @@ function ContractButton({signer, contract, setContract, contractAddress, setCont
                 value={contractAddress}
                 onKeyPress={event => {
                   if (event.key === 'Enter') {
-                    connectContract(signer, contract, setContract, contractAddress, setContractAddress);
+                    connectContract(signer, contract, setContract, contractAddress, setContractBalance, provider, setRole, address);
                     onClose();
                   }
                 }}
@@ -91,7 +105,7 @@ function ContractButton({signer, contract, setContract, contractAddress, setCont
             <Button
               mr={3}
               onClick={() => {
-                connectContract(signer, contract, setContract, contractAddress, setContractAddress);
+                connectContract(signer, contract, setContract, contractAddress, setContractAddress, provider, setRole, address);
                 onClose();
               }}>
                 OK
@@ -104,7 +118,7 @@ function ContractButton({signer, contract, setContract, contractAddress, setCont
   );
 }
 
-function displayAddress(address) {
+function formatAddress(address) {
   return ethers.utils.isAddress(address)
   ? `${address.slice(0, 6)}...${address.slice(
       address.length - 4,
@@ -112,35 +126,72 @@ function displayAddress(address) {
     )}`
   : '';
 }
+
+async function getEthBalance(signer, provider, address) {
+  const formaterETH = new Intl.NumberFormat(undefined, {
+    maximumFractionDigits: 5,
+  }).format
+  
+  let balance = "";
+  if (provider === null) {
+    balance = ethers.utils.formatUnits(await signer.getBalance());
+  } else {
+    balance = ethers.utils.formatUnits(await provider.getBalance(address));
+  }
+  return formaterETH(balance);
+}
     
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [provider, setProvider] = useState(null);
   const [signer, setSigner] = useState(null);
   const [address, setAddress] = useState("");
   const [contract, setContract] = useState(null);
   const [contractAddress, setContractAddress] = useState("");
+  const [walletBalance, setWalletBalance] = useState("");
+  const [contractBalance, setContractBalance] = useState("");
+  const [role, setRole] = useState("none")
 
   return (
     <Flex as="nav" alignItems="center" padding="14px" borderBottomWidth="1px" fontSize='xl'>
       <Button>
         <HamburgerIcon />
       </Button>
+      <Text marginLeft="14px">
+        Wallet:
+      </Text>
       <Button marginLeft="14px" onClick={() => {
           if (address === "") {
-            connectWallet(setSigner, setAddress);
+            connectWallet(setSigner, setAddress, setWalletBalance, setProvider, setProvider);
           } else {
             console.log("already connected:", address);
           }
         }}>
-        {address === "" ? "Connect Wallet" : displayAddress(address)}
+        {address === "" ? "Connect" : formatAddress(address)}
       </Button>
+      <Text fontSize='sm' marginLeft="14px">{walletBalance ? "ETH" : ""}</Text>
+      <Box marginLeft="8px"/>
+      <Text>{walletBalance}</Text>
+      <Text marginLeft="28px">
+        Contract:
+      </Text>
       <ContractButton
         signer={signer}
         contract={contract}
         setContract={setContract}
         contractAddress={contractAddress}
         setContractAddress={setContractAddress}
+        setContractBalance={setContractBalance}
+        provider={provider}
+        setRole={setRole}
+        address={address}
       />
+      <Text fontSize='sm' marginLeft="14px">{contractBalance ? "ETH" : ""}</Text>
+      <Box marginLeft="8px"/>
+      <Text>{contractBalance}</Text>
+      <Text marginLeft="28px">
+        Role: {role}
+      </Text>
       <Spacer />
       <Button onClick={toggleColorMode}>
         {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
