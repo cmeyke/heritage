efe31c38bc4eee702561f2b9e92f20a991ba05c0
Carsten Meyke <carsten.meyke@gmail.com>
2023-02-05 12:17:25 +0100

    Dashboard refactoring

--- heritage-app/src/components/Dashboard.jsx
+++ heritage-app/src/components/Dashboard.jsx
@@ -1,51 +1,51 @@
 import {
-  Box,
   Button,
   Heading,
-  SimpleGrid,
+  Grid,
   Table,
   TableContainer,
   Tbody,
   Td,
   Text,
-  Tr
+  Tr,
+  GridItem
 } from "@chakra-ui/react";
 
 export default function Dashboard({role, alive, timeAlive}) {
   return (
-    <SimpleGrid
-      minChildWidth="400px"
+    <Grid
+      templateColumns="repeat(3, 1fr)"
       padding="14px"
       spacing={10}
     >
-      <Box w="400px" fontSize='xl'>
-        <Heading>
-          Contract Properties
-        </Heading>
-        <TableContainer padding="14px">
-          <Table>
-            <Tbody>
-              <Tr>
-                <Td><Text>Role</Text></Td>
-                <Td><Text>{role}</Text></Td>
-              </Tr>
-              <Tr>
-                <Td><Text>Alive Interval</Text></Td>
-                <Td><Text>{timeAlive} {timeAlive === "1" ? "Day" : "Days"}</Text></Td>
-              </Tr>
-              <Tr>
-                <Td><Text>Alive left</Text></Td>
-                <Td><Button><Text fontSize='xl'>{alive} {alive === "1" ? "Day" : "Days"}</Text></Button></Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
-      <Box w="400px" fontSize='xl'>
+      <GridItem colSpan="1" fontSize='xl' textAlign="left">
+          <Heading marginLeft="28px">
+            Contract Properties
+          </Heading>
+          <TableContainer padding="14px">
+            <Table>
+              <Tbody>
+                <Tr>
+                  <Td><Text>Role</Text></Td>
+                  <Td><Text>{role}</Text></Td>
+                </Tr>
+                <Tr>
+                  <Td><Text>Alive Interval</Text></Td>
+                  <Td><Text>{timeAlive} {timeAlive === "1" ? "Day" : "Days"}</Text></Td>
+                </Tr>
+                <Tr>
+                  <Td><Text>Alive left</Text></Td>
+                  <Td><Button><Text fontSize='xl'>{alive} {alive === "1" ? "Day" : "Days"}</Text></Button></Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+      </GridItem>
+      <GridItem colSpan="2"  fontSize='xl' textAlign="left">
         <Heading>
           Send Ether
         </Heading>
-      </Box>
-    </SimpleGrid>
+      </GridItem>
+    </Grid>
   )
 }
